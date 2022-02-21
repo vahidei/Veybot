4e7a9f986294667c45de03920ba8863a53be5ff1
vahidey <vahidey.dev@gmail.com>
2022-02-21 22:56:40 +0330

    add app

--- server.js
+++ server.js
@@ -2,9 +2,16 @@ const express = require('express');
 const socketIO = require('socket.io');
 
 const PORT = process.env.PORT || 3000;
+const INDEX = '/index.html';
+const app = express();
 
-const server = express()
+const server = app
+  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
+
+app.get('/', (req, res) => {
+  res.send('<h1>Hello world</h1>');
+});
 
 const io = socketIO(server);
 
