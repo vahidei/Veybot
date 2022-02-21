607f59fc32f448b935f6798fba1fa4171ca296b8
vahidey <vahidey.dev@gmail.com>
2022-02-21 23:44:15 +0330

    send message

--- server.js
+++ server.js
@@ -16,6 +16,10 @@ const io = socketIO(server);
 io.on('connection', (socket) => {
   console.log('Client connected');
   socket.on('disconnect', () => console.log('Client disconnected'));
+  socket.on('send message', () => {
+      socket.emit('result', 'pm received');
+  });
 });
+
 
 setInterval(() => io.emit('time', new Date().toTimeString()), 1000);
