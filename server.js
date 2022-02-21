4542c47a98ddfb15ac33a877833cc342815526fb
vahidey <vahidey.dev@gmail.com>
2022-02-21 22:57:58 +0330

    change sever

--- server.js
+++ server.js
@@ -5,9 +5,7 @@ const PORT = process.env.PORT || 3000;
 const INDEX = '/index.html';
 const app = express();
 
-const server = app
-  .use((req, res) => res.sendFile(INDEX, { root: __dirname }))
-  .listen(PORT, () => console.log(`Listening on ${PORT}`));
+const server = app.listen(PORT, () => console.log(`Listening on ${PORT}`));
 
 app.get('/', (req, res) => {
   res.send('<h1>Hello world</h1>');
